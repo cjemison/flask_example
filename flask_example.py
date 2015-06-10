1680e401baf9167b8a0c7e1e6938a9522c1a1e4c
Cornelius Jemison <cornelius.jemison@gmail.com>
2015-06-09 21:01:57 -0500

    added rest endpoint

--- flask_example.py
+++ flask_example.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.pymongo import PyMongo
 from flask import render_template
 from werkzeug.contrib.cache import MemcachedCache
+from flask_restful import Resource, Api
 
 cache = MemcachedCache(['127.0.0.1:11211'])
 
@@ -11,7 +12,12 @@ app.config['MONGO_HOST'] = '127.0.0.1'
 app.config['MONGO_USERNAME'] = 'cjemison'
 app.config['MONGO_PASSWORD'] = 'cjemison'
 mongo = PyMongo(app)
+api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
 @app.route('/')
 def hello_world():
@@ -23,6 +29,8 @@ def hello_world():
         cache.set('my-item', rv, timeout=5 * 60)
     return render_template('index.html', name=rv)
 
+
+api.add_resource(HelloWorld, '/r/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
